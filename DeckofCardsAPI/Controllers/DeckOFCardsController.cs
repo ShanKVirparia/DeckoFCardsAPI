9d4b2ab1222ed70dbc53dfe01f6d62e8faca64fb
ShanKVirparia <shan.k.virparia@gmail.com>
2023-01-29 14:17:03 -0700

    First Commit

---
+++ DeckofCardsAPI/Controllers/DeckOFCardsController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using DeckofCardsAPI.Models;
+
+namespace DeckofCardsAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DeckOFCardsController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IEnumerable<Card>> GetCards()
+        {
+            using (var client = new HttpClient())
+            {
+                // Make a call to the API to generate a new deck
+                var deckUrl = "https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1";
+                var response = await client.GetAsync(deckUrl);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var content = await response.Content.ReadAsStringAsync();
+                    dynamic deck = JsonConvert.DeserializeObject(content);
+                    string deckId = deck.deck_id;
+
+                    // Draw 5 cards from the deck
+                    var drawUrl = $"https://deckofcardsapi.com/api/deck/{deckId}/draw/?count=5";
+                    response = await client.GetAsync(drawUrl);
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        content = await response.Content.ReadAsStringAsync();
+                        dynamic cards = JsonConvert.DeserializeObject(content);
+                        var result = new List<Card>();
+                        foreach (var card in cards.cards)
+                        {
+                            result.Add(new Card
+                            {
+                                Value = card.value,
+                                Suit = card.suit,
+                                Image = card.image
+                            });
+                        }
+                        return result;
+                    }
+                }
+            }
+            return null;
+        }
+    }
+}
---
+++ DeckofCardsAPI/Models/Card.cs
@@ -0,0 +1,10 @@
+﻿using DeckofCardsAPI;
+namespace DeckofCardsAPI.Models
+{
+    public class Card
+    {
+        public string Value { get; set; }
+        public string Suit { get; set; }
+        public string Image { get; set; }
+    }
+}
